f9fbe85a31f4c55a3b5de30a853f2423f47d44cf
slezak83 <f22b2honda@gmail.com>
2017-12-07 19:18:59 -0800

    Add files via upload
    Updated Main.Java, Controller.Java, Added a new background photo for Main.Java

--- Controller.java
+++ Controller.java
@@ -1,32 +1,33 @@
 package sample;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.*;
-
-import static java.lang.System.getProperty;
-
 public class Controller extends Parent {
-    Button b = new Button("Search");
-    TextField search1 = new TextField();
-    Stage stage = new Stage();
-    Scene scene1= new Scene(this, 900, 575);
-    File file = new File(getProperty("user.home"), "/Desktop/Toyota References and TSB's/CSP WEP/new.txt");
+    @FXML
+    public TextField search1;
+    @FXML
+    public Button search;
+    @FXML
+    Stage window = new Stage();
+    @FXML
+    public Scene scene1;
 
-    public void setOnAction(ActionEvent actionEvent) throws IOException {
-        FileInputStream inputStream = new FileInputStream(file);
-        scene1.getStylesheets().add(Controller.class.getResource("actionevent.css").toExternalForm());
-        stage.setScene(scene1);
-        stage.show();
-
-        if (!(actionEvent.getSource() == b)) {
-            b.setText(search1.getText());
-            return;
+    @FXML
+    public void setOnAction() {
+        if (search1.getText().isEmpty()) {
+            System.out.println("Please Enter Text In The Search Field");
+        } else {
+            if (search1.getText().contains("TSB")) {
+                window.setScene(scene1);
+                window.show();
+            }else{
+                    System.out.println("Unable to Locate Results of Your Search, Try Again");
+                }
+            }
         }
     }
-}
--- Main.java
+++ Main.java
@@ -13,7 +13,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Main extends Application implements EventHandler<ActionEvent>{
     Button b = new Button("Search");
     TextField search1 = new TextField();
     GridPane grid = new GridPane();
@@ -24,12 +28,14 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         /* FXML loader file loads controller file */
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Toyota Reference Information Search");
-        Scene scene= new Scene(root, 900, 575);
+        Scene scene = new Scene(root, 900, 575);
         scene.getStylesheets().add(Main.class.getResource("project.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        /* Gridpane properties */
+
+
+        /* gridPane properties */
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(5);
         grid.setHgap(5);
@@ -37,17 +43,35 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         /* Label */
         grid.getChildren().add(label);
 
-        /* textfield properties */
+        /* textField properties */
         search1.setPromptText("Toyota Docs");
         search1.setPrefSize(299, 25);
         GridPane.setConstraints(search1, 20, 20);
         grid.getChildren().add(search1);
         b.setOnAction(this::handle);
+        search1.setOnAction(this::handle);
+
+        /*Path for file to be read
+        * bufferedReader to store path variable*/
+        String location = "C:\\Users\\f22b2\\IdeaProjects\\ToyotaInfoApp\\src\\sample\\";
+        String fileName = "HELLO";
+        File file = new File(location+fileName);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
     }
+    /*Action event being performed
+    * and is linked with controller and FXML file
+    * to process event occurring. Attempted
+    * to create another Java class file to
+    * handle second part of event for I/O*/
     @Override
     public void handle(ActionEvent event) {
+            if (event.getSource().equals(b));{
+        }
+            if (event.getSource().equals(search1));{
 
+        }
     }
+
     public static void main(String[] args) {
         launch(args);
     }
